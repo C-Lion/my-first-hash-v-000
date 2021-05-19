232c15f8ed2a98aa3bee3d12c5473e22026d9d8a
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-19 20:53:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = { "my_program" => "Online Softare Engineering"}
+  my_hash = { "my_program" => "Full Stack Web Dev V8"}
   
 end
 
